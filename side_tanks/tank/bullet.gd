b5e92e4dbe0fadb2553ad5416d2aabe5b0aa0f6e
Ruben Teijeiro <Ruben.TEIJEIRO-SUAREZ@ext.ec.europa.eu>
2024-11-22 18:21:39 +0100

    Adds aiming and explosions to Side Tanks game.

--- side_tanks/tank/bullet.gd
+++ side_tanks/tank/bullet.gd
@@ -1,22 +1,37 @@
 extends Area2D
 
 @onready var _visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+@onready var _animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 var direction := Vector2.ZERO
 var velocity := Vector2(350, 0)
-var speed := 800
+var speed
+var bullet_gravity = 980
 
 func _ready() -> void:
+	_animated_sprite_2d.hide()
 	_visible_on_screen_notifier_2d.screen_exited.connect(func() -> void:
-		queue_free()
+		pass
+		#queue_free()
 	)
 	body_entered.connect(func(body: Node2D) -> void:
-		queue_free()
+		_explode()
 	)
 
 	velocity = direction.normalized() * speed
 
 func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
+	velocity.y += bullet_gravity * delta
 	position += velocity * delta
 	rotation = velocity.angle()
+
+func _explode() -> void:
+	$Sprite2D.hide()
+	velocity = Vector2.ZERO
+	bullet_gravity = 0
+	_animated_sprite_2d.show()
+	_animated_sprite_2d.play("default")
+	_animated_sprite_2d.animation_finished.connect(func() -> void:
+		queue_free()
+	)
+	
--- side_tanks/tank/tank.gd
+++ side_tanks/tank/tank.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
-var _crosshair = preload("res://side_tanks/resources/tank/crosshair184.png")
 var Bullet = preload("res://side_tanks/tank/bullet.tscn")
 
 @onready var _turret: Node2D = $Turret
 @onready var _muzzle: Marker2D = $Turret/Muzzle
+@onready var _line_2d: Line2D = $Line2D
 
-const SPEED = 100.0
+const SPEED := 100.0
+var max_points := 250
+var bullet_speed := 800
+var can_shoot := false
 
-func _ready() -> void:
-	Input.set_custom_mouse_cursor(_crosshair)
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -24,22 +25,39 @@ func _physics_process(delta: float) -> void:
 
 	# Turret rotation.
 	var mouse_position := get_global_mouse_position()
-	var direction_to_mouse := _turret.global_position.direction_to(mouse_position)
+	var direction_to_mouse := _muzzle.global_position.direction_to(mouse_position)
 	var angle_to_mouse := direction_to_mouse.angle()
 	angle_to_mouse = clamp(angle_to_mouse, -PI/2, 0.0)
 	_turret.rotation = angle_to_mouse
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+	
+	# Aim and shoot.
+	if Input.is_action_pressed("click"):
+		can_shoot = true
+		_line_2d.show()
+		update_trajectory(delta)
+		
+	if Input.is_action_just_released("click"):
+		if can_shoot:
+			can_shoot = false
+			_line_2d.hide()
 			_shoot()
-			
 			
 func _shoot() -> void:
 	var bullet:Area2D = Bullet.instantiate()
 	bullet.position = _muzzle.global_position
 	bullet.rotation = _turret.rotation
 	bullet.direction = _turret.global_position.direction_to(_muzzle.global_position)
+	bullet.speed = bullet_speed
 	get_tree().root.add_child(bullet)
 	
+
+func update_trajectory(delta):
+	_line_2d.clear_points()
+	var target_position = to_local(_muzzle.global_position)
+	var target_velocity = _muzzle.global_transform.x * bullet_speed
+	for i in max_points:
+		_line_2d.add_point(target_position)
+		target_velocity.y += get_gravity().y * delta
+		target_position += target_velocity * delta
+		if target_position.y > 0:
+			break
